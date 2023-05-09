df6a1607d55d38ede06140141d90d1401279f8a1
midnight <ivanator666@gmail.com>
2023-05-10 00:26:14 +0300

    made plain test

--- tests/gendiff_test.py
+++ tests/gendiff_test.py
@@ -1,7 +1,11 @@
 from gendiff.difference_generator import generate_diff
-from tests.fixtures.right_result import RIGHT_RESULT, RIGHT_NESTED
+from gendiff.formatters.stylish import gen_dict_string
+from gendiff.formatters.plain import gen_plain_string
+from tests.fixtures.right_result import RIGHT_RESULT, RIGHT_NESTED, RIGHT_STYLISH, RIGHT_STYLISH_NESTED, RIGHT_PLAIN, RIGHT_PLAIN_NESTED
 
 
+
+# Dictionary tests
 def test_gendiff_json():
     assert RIGHT_RESULT == generate_diff(
         'tests/fixtures/file1.json', 'tests/fixtures/file2.json'
@@ -17,4 +21,36 @@ def test_gendiff_yml():
 def test_gendiff_nested():
     assert RIGHT_NESTED == generate_diff(
         'tests/fixtures/nested1.json', 'tests/fixtures/nested2.json'
+    )
+
+
+# Stylish tests
+def test_stylish_json():
+    assert RIGHT_STYLISH == gen_dict_string(generate_diff(
+        'tests/fixtures/file1.json', 'tests/fixtures/file2.json')
+    )
+
+
+def test_stylish_yaml():
+    assert RIGHT_STYLISH == gen_dict_string(generate_diff(
+        'tests/fixtures/file1.yaml', 'tests/fixtures/file2.yml')
+    )
+
+
+def test_stylish_nested():
+    assert RIGHT_STYLISH_NESTED == gen_dict_string(generate_diff(
+        'tests/fixtures/nested1.json', 'tests/fixtures/nested2.json')
+    )
+
+
+# Plain tests
+def test_plain():
+    assert RIGHT_PLAIN == gen_plain_string(generate_diff(
+        'tests/fixtures/file1.json', 'tests/fixtures/file2.json')
+    )
+
+
+def test_plain_nested():
+    assert RIGHT_PLAIN == gen_plain_string(generate_diff(
+        'tests/fixtures/nested1.json', 'tests/fixtures/nested2.json')
     )
