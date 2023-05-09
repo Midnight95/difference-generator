e6293be44eae8c5969c9820f938d7a4d5623efac
midnight <ivanator666@gmail.com>
2023-05-09 12:10:24 +0300

    added stylish formater

---
+++ gendiff/formatters/stylish.py
@@ -0,0 +1,12 @@
+def stylish(_dict):
+    def _iter(item, depth):
+        spaces = 4 * ' '
+        result = '\n'
+        for key, val in item.items():
+            if isinstance(val, dict):
+                result += f'{spaces * depth}{key}: ' \
+                          f'{{{_iter(val, depth + 1)}{spaces * (depth + 1)}}}\n'
+            else:
+                result += f'{spaces * depth}{key}: {val}\n'
+        return result
+    return '{' + _iter(_dict, 0) + '}'
