11ccbe32a9902f2f4e520f6aa57017fe46578cc6
midnight <ivanator666@gmail.com>
2023-05-09 12:09:32 +0300

    update difference_generator

--- gendiff/difference_generator.py
+++ gendiff/difference_generator.py
@@ -5,18 +5,17 @@ def is_dict(item):
     return isinstance(item, dict)
 
 
-def update_values(item):
-    if is_dict(item):
-        for key, value in item.items():
-            if isinstance(value, bool):
-                item[key] = str(value).lower()
-            elif isinstance(value, type(None)):
-                item[key] = 'null'
-            if is_dict(value):
-                update_values(value)
+def stringify_values(item):
+    for key, value in item.items():
+        if isinstance(value, bool):
+            item[key] = str(value).lower()
+        elif isinstance(value, type(None)):
+            item[key] = 'null'
+        if is_dict(value):
+            stringify_values(value)
 
 
-def get_keys(first_item, second_item):
+def get_keys(first_item, second_item) -> set:
     if is_dict(first_item) and is_dict(second_item):
         keys = first_item.keys() | second_item.keys()
     elif is_dict(first_item):
@@ -26,59 +25,57 @@ def get_keys(first_item, second_item):
     return keys
 
 
-def make_diff(first_item: dict, second_item: dict) -> dict:
+def add_spaces(_dict: dict) -> dict:
+    result = {}
+    for key, value in _dict.items():
+        result[f'    {key}'] = value
+        if is_dict(value):
+            add_spaces(value)
+
+    return result
+
+
+def _iter(first_item: dict, second_item: dict) -> dict: # noqa 901
 
     result = {}
-
     keys = get_keys(first_item, second_item)
 
     for key in sorted(keys):
         first_value = first_item.get(key) if is_dict(first_item) else None
         second_value = second_item.get(key) if is_dict(second_item) else None
 
-        if any(map(lambda x: is_dict(x), (first_value, second_value))):
-            result[f'    {key}'] = make_diff(first_value, second_value)
-
-        elif first_value == second_value:
-            result[f'    {key}'] = first_value
+        if is_dict(first_value) and is_dict(second_value):
+            result[f'    {key}'] = _iter(first_value, second_value)
+        # Only first value is present
         elif second_value is None:
-            result[f'  - {key}'] = first_value
+            if is_dict(first_value):
+                result[f'  - {key}'] = add_spaces(first_value)
+            else:
+                result[f'  - {key}'] = first_value
+        # Only second value is present
         elif first_value is None:
-            result[f'  + {key}'] = second_value
+            if is_dict(second_value):
+                result[f'  + {key}'] = add_spaces(second_value)
+            else:
+                result[f'  + {key}'] = second_value
+        # Both values are present and identical
+        elif first_value == second_value:
+            if is_dict(first_value):
+                result[f'    {key}'] = _iter(first_value, second_value)
+            else:
+                result[f'    {key}'] = first_value
+        # Both values are present but different
         else:
             result[f'  - {key}'] = first_value
             result[f'  + {key}'] = second_value
+
     return result
 
 
-def generate_diff(first_file: str, second_file: str) -> str:
+def generate_diff(first_file: str, second_file: str) -> dict:
     first_file, second_file = parse_extensions(first_file, second_file)
 
-    update_values(first_file)
-    update_values(second_file)
+    stringify_values(first_file)
+    stringify_values(second_file)
 
-    def stringify(item, depth):
-        spaces = '   '
-        result = '\n'
-        for key, val in item.items():
-            if is_dict(val):
-                result += f'{spaces * depth}{key}: ' \
-                          f'{{{stringify(val, depth + 1)}{spaces * (depth + 1)}}}\n'
-            else:
-                result += f'{spaces * depth}{key}: {val}\n'
-        return result
-
-    return '{' + stringify(make_diff(first_file, second_file), 0) + '}'
-
-
-# # Нужно добавить условие, чтобы не
-# first_file = '/home/kenny/dev/python-project-50/tests/fixtures/nested1.json'
-# second_file = '/home/kenny/dev/python-project-50/tests/fixtures/nested2.json'
-#
-# first_file, second_file = parse_extensions(first_file, second_file)
-#
-# print(make_diff(first_file, second_file))
-
-first_file = '/home/kenny/dev/python-project-50/tests/fixtures/nested1.json'
-second_file = '/home/kenny/dev/python-project-50/tests/fixtures/nested2.json'
-print(generate_diff(first_file, second_file))
+    return _iter(first_file, second_file)
