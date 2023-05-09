707acc89752fb0af1a560e9560a2d7813101f451
midnight <ivanator666@gmail.com>
2023-05-09 17:43:25 +0300

    code cleaning

--- gendiff/parser.py
+++ gendiff/parser.py
@@ -12,7 +12,9 @@ def parse():
     parser.add_argument('first_file', type=str)
     parser.add_argument('second_file', type=str)
     parser.add_argument(
-        '-f', '--format', help='set format of output', default='stylish'
+        '-f', '--format',
+        help='set format of output',
+        default='stylish'
     )
 
     return parser.parse_args()
