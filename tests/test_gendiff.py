86980fbcfbfe5a38842ef8fb987f29aa4ad8920e
Midnight95 <ivanator666@gmail.com>
2023-05-18 11:29:05 +0300

    updated tests

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -13,12 +13,13 @@ nested_json_2 = 'tests/fixtures/nested2.json'
 linear = 'tests/fixtures/linear'
 stylish = 'tests/fixtures/nested_stylish'
 plain = 'tests/fixtures/nested_plain'
-
+json = 'tests/fixtures/json.json'
 
 @pytest.mark.parametrize('path1, path2, format_name, expected', [(linear_json_1, linear_json_2, 'stylish', linear),
                                                                  (linear_yml_1, linear_yaml_2, 'stylish', linear),
                                                                  (nested_json_1, nested_json_2, 'stylish', stylish),
-                                                                 (nested_json_1, nested_json_2, 'plain', plain)]
+                                                                 (nested_json_1, nested_json_2, 'plain', plain),
+                                                                 (nested_json_1, nested_json_2, 'json', json)]
                          )
 def test_generate_diff(path1, path2, format_name, expected):
     with open(expected) as expectation:
